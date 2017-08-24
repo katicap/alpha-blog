833878982d0d64f5863ea631d1d2bb12cea49c6b
Rails Udemy Course <railsudemycourse@gmail.com>
2017-08-24 17:32:13 +0000

    Singup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
  root 'pages#home'
  
  
- get 'about', to:'pages#about'
+ get 'about', to: 'pages#about'
  
  resources :articles
+ 
+ get 'signup', to: 'users#new'
+ resources :users, except: [:new]
   
   
 end
